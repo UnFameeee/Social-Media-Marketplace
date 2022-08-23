8ffadb7d3b545b96742cd88265afe19aaf64cdac
duyduong <junloop888@gmail.com>
2022-08-24 01:09:27 +0700

    Adding navbar

--- frontend/src/components/Navbar/NavigationBar.js
+++ frontend/src/components/Navbar/NavigationBar.js
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Home,
+  Storefront,
+  LiveTv,
+  Groups,
+  SportsEsports,
+} from "@mui/icons-material";
 function NavigationBar() {
   return (
-    <div>NavigationBar</div>
-  )
+    <div className="flex items-center px-5">
+      <div className="rightNav">
+        <span>Logo</span>
+        <div className="dropDown"></div>
+      </div>
+      <div className="centerNav ">
+        <ul className="listNav flex gap-[10px]">
+          <li>
+            <Home className="navBarIcon" style={{ fontSize: 40 }} />
+          </li>
+          <li>
+            <Storefront className="navBarIcon" style={{ fontSize: 40 }} />{" "}
+          </li>
+          <li>
+            <LiveTv className="navBarIcon" style={{ fontSize: 40 }} />
+          </li>
+          <li>
+            <Groups className="navBarIcon" style={{ fontSize: 40 }} />
+          </li>
+          <li>
+            <SportsEsports className="navBarIcon" style={{ fontSize: 40 }} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
