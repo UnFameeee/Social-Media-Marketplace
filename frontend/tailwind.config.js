6c7bb911894a166df80644e935650190f943118f
duyduong <junloop888@gmail.com>
2022-08-26 00:14:32 +0700

    complete basic navigation bar and add some more color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue8f3': "#1a78f3",
+        'greyf1':'#f1f1f1',
+      },
+    },
   },
   plugins: [],
-}
+};
