0d7f67eeac0067496833adce1d8a032ba85008bd
Quốc Thắng <quocthang036@gmail.com>
2022-08-31 23:13:13 +0700

    Last Commit for BE in V1.0
    Last Commit for BE in V1.0

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiParam } from '@nestjs/swagger';
+import { Profile } from 'src/social-media/profile/model/profile.model';
 import { AuthService } from './auth.service';
 import { RegisterProfileDto } from './dto/register-profile.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
@@ -15,6 +17,39 @@ export class AuthController {
         return this.authService.register(registerProfileDto);
     }
 
+    // @ApiParam({
+    //     name: "test",
+    //     description: "This Decorator specifies the documentation for a specific Parameter, in this case the <b>name</b> Param.",
+    //     allowEmptyValue: false,
+    //     examples: {
+    //         a: {
+    //             summary: "Name is Pete",
+    //             description: "Pete can be provided as a name. See how it becomes a selectable option in the dropdown",
+    //             value: "Pete"
+    //         },
+    //         b: {
+    //             summary: "Name is Joe",
+    //             value: "Joe"
+    //         }
+    //     }
+    // })
+    @ApiBody({
+        type: Profile,
+        description: "Login API. This will need {email, hashPassword} to use. The reponse will be access_token, refresh_token",
+        examples: {
+            ex1: {
+                summary: "Empty Data",
+                description: "{ 'email': 'test12@gmail.com', 'hashPassword': 'Lmao123!!!'}",
+                value: {} as Profile
+            },
+            ex2: {
+                summary: "Sample Data",
+                description: "Sample input for this API",
+                value: {email: "test12@gmail.com", hashPassword: "Lmao123!!!"} as Profile
+            }
+        }
+    
+    })
     @UseGuards(AuthGuard('local'))
     @HttpCode(200)
     @Post("/login")
