9b512a7edec518264ca02d47361f60d828545ff6
vunh <59404796+vu2872001@users.noreply.github.com>
2022-09-01 23:32:19 +0700

    Screen Login & Register ver 1.1
    BackEnd: not test yet.
    
    FrontEnd:
    - More COMPONENTS (for form only)
    - Use Formik to validate form
    - Only need to call API from BackEnd

---
+++ frontend/src/components/Form/index.js
@@ -0,0 +1,17 @@
+import InputForm from './InputForm'
+import SelectForm from './SelectForm'
+import DateTimePickerForm from './DateTimePickerForm'
+import CheckBoxForm from './CheckBoxForm'
+import ButtonForm from './ButtonForm'
+import PasswordInputForm from './PasswordInputForm'
+
+const CustomForm = {
+    InputForm,
+    SelectForm,
+    DateTimePickerForm,
+    CheckBoxForm,
+    ButtonForm,
+    PasswordInputForm,
+}
+
+export default CustomForm;
