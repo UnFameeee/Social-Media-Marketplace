54c6ab8b78bbc4116e26017f26a029dde9ce05b1
Quốc Thắng <quocthang036@gmail.com>
2022-09-07 00:12:24 +0700

    Fix bug login return false value.
    fix bug (07/09/2022)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: "itsavedaworld-SMM"
+  });
 
 
 
