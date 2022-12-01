4f8917fbd3c2a9882de2a99ab919beaacca63750
DuongDuy <59405786+Duyduong689@users.noreply.github.com>
2022-12-01 16:47:08 +0700

    create-button-to-navigate-to-order-for-buyer-and-seller

--- frontend/src/screens/Marketplace/MarketplaceNavbar.jsx
+++ frontend/src/screens/Marketplace/MarketplaceNavbar.jsx
@@ -1,4 +1,4 @@
-import React, {  useMemo } from "react";
+import React, { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { ShoppingBag } from "@mui/icons-material";
 import SellIcon from "@mui/icons-material/Sell";
@@ -14,6 +14,11 @@ function MarketplaceNavbar() {
     var result = Helper.checkURL("selling", {}, true);
     return result;
   });
+  const isOrderPurchasedActive = useMemo(() => {
+    var result = Helper.checkURL("orderpurchased", {}, true);
+    return result;
+  });
+
   const handleNavigateToShopping = () => {
     if (!isShoppingActive) {
       navigate("/shopping");
@@ -24,38 +29,58 @@ function MarketplaceNavbar() {
       navigate("/selling");
     }
   };
+  const handleNavigateToOrderPurchased = () => {
+    if (!isOrderPurchasedActive) {
+      navigate("/orderpurchased");
+    }
+  };
+
   return (
     <div className="navigation flex gap-[0.5rem] flex-col mb-[1rem] p-[0.5rem] bg-gray-100 rounded-3xl shadow-lg">
-    <MUI.ButtonWithIcon
-      onClick={handleNavigateToShopping}
-      style={{
-        display: "flex",
-        gap: "10px",
-        justifyContent: "start",
-        border: isShoppingActive ? "2px solid var(--primary-color)" : 0,
-      }}
-    >
-      <ShoppingBag
-        style={{ fontSize: "2.5rem", color: "var(--primary-color)" }}
-      />
-      <span className=" text-[2rem]">Shopping</span>
-    </MUI.ButtonWithIcon>
-    <MUI.ButtonWithIcon
-      onClick={handleNavigateToSelling}
-      style={{
-        display: "flex",
-        gap: "10px",
-        justifyContent: "start",
-        border: isSellingActive ? "2px solid var(--primary-color)" : 0,
-      }}
-    >
-      <SellIcon
-        style={{ fontSize: "2.5rem", color: "var(--primary-color)" }}
-      />
-      <span className=" text-[2rem]">Selling</span>
-    </MUI.ButtonWithIcon>
-  </div>
-  )
+      <MUI.ButtonWithIcon
+        onClick={handleNavigateToShopping}
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "start",
+          border: isShoppingActive ? "2px solid var(--primary-color)" : 0,
+        }}
+      >
+        <ShoppingBag
+          style={{ fontSize: "2.5rem", color: "var(--primary-color)" }}
+        />
+        <span className=" text-[2rem]">Shopping</span>
+      </MUI.ButtonWithIcon>
+      <MUI.ButtonWithIcon
+        onClick={handleNavigateToSelling}
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "start",
+          border: isSellingActive ? "2px solid var(--primary-color)" : 0,
+        }}
+      >
+        <SellIcon
+          style={{ fontSize: "2.5rem", color: "var(--primary-color)" }}
+        />
+        <span className=" text-[2rem]">Selling</span>
+      </MUI.ButtonWithIcon>
+      <MUI.ButtonWithIcon
+        onClick={handleNavigateToOrderPurchased}
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "start",
+          border: isOrderPurchasedActive ? "2px solid var(--primary-color)" : 0,
+        }}
+      >
+        <SellIcon
+          style={{ fontSize: "2.5rem", color: "var(--primary-color)" }}
+        />
+        <span className=" text-[2rem]">Order Purchased</span>
+      </MUI.ButtonWithIcon>
+    </div>
+  );
 }
 
-export default MarketplaceNavbar
+export default MarketplaceNavbar;
